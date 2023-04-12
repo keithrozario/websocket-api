a13d8964b6b2a5f6809bfe3734dfcee7eb195e33
Keith Rozario <keithjosephrozario@gmail.com>
2023-04-12 14:25:46 +0800

    used variable instead of hardcode api"

--- custom_foo.py
+++ custom_foo.py
@@ -1,16 +1,18 @@
 import boto3
+import os
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
 logger = Logger()
-endpoint_url = "https://0gmpnpncdl.execute-api.ap-southeast-1.amazonaws.com/dev"
-client = boto3.client('apigatewaymanagementapi',endpoint_url=endpoint_url)
+
+endpoint_http_url = os.environ['API_ENDPOINT'].replace("wss://","https://")
+
+client = boto3.client('apigatewaymanagementapi',endpoint_url=endpoint_http_url)
 
 @logger.inject_lambda_context
 def main(event:dict , context: LambdaContext):
     logger.info(event)
     connection_id = event.get('requestContext').get('connectionId')
-    logger.info(connection_id)
 
     client.post_to_connection(
         Data=b'barbarbarbarbarbar',
